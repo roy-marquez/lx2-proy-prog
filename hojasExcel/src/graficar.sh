829f4a96c85b3e4d3b8db886ab12bbd84c18ac66
Roy Marquez <romasi@gmail.com>
2016-04-03 19:10:35 -0600

    Comenté y modifique el script graficar.sh los archivos de salidad formateada ahora son .dat

--- hojasExcel/src/graficar.sh
+++ hojasExcel/src/graficar.sh
@@ -2,15 +2,20 @@
 
 # Este script extrae archivos de excel y grafica datos.
 
+#esta linea indica el PATH de origen de los datos xls
 DATOS=../hojasDatos
 
-
+#esta linea indica la subcarpeta donde se crearan los cvs
 SALIDA_DATOS=$DATOS/datos_csv
 
+#esta linea crea la subcarpeta datos_csv donde se guardan los datos
 mkdir $SALIDA_DATOS
 
+#inicia el contador en cero
 M=0
 
+#este ciclo extrae la informacion de los .xls y los convierte a cvs
+#la salida de error la direcciona al archivo error1.log
 for archivo in `find $DATOS -name "*.xls"`
 do
 	echo "Procesando archivo $archivo"
@@ -18,14 +23,18 @@ do
 	let M=M+1
 done 2>error1.log
 
+#setea de nuevo el contador a cero
 M=0
 
+#este ciclo extrae y limpia los datos desde los cvs y los deja en archivos graf-*.dat
+#la salida de error la direcciona al archivo error2.log
 for archivo in `find $SALIDA_DATOS -name "*.csv"`
 do
 	echo "Dando formato al archivo de datos: $archivo"
-	cat $archivo  | awk -F "\",\"" '{print $1 " " $2 " " $3 " " $4 " "  $5}' | grep -v Sensor | sed '1,$ s/"//g' > $SALIDA_DATOS/dato-$M.out
+	cat $archivo  | awk -F "\",\"" '{print $1 " " $2 " " $3 " " $4 " "  $5}' | grep -v Sensor | sed '1,$ s/"//g' > $SALIDA_DATOS/graf-$M.dat
 	
 	let M=M+1
 done 2> error2.log
+
 
 
